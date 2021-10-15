b6851dbbcd315e6f90da6c00bb88f9af03f0fbc1
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-10-15 09:27:56 +0200

    add flash in template index

--- server.py
+++ server.py
@@ -28,10 +28,11 @@ def index():
 def showSummary():
     try:
         club = [club for club in clubs if club['email'] == request.form['email']][0]
+        return render_template('welcome.html',club=club,competitions=competitions)
     except IndexError:
         flash("The email isn't found !")
         return render_template('index.html')
-    return render_template('welcome.html',club=club,competitions=competitions)
+
 
 
 @app.route('/book/<competition>/<club>')
